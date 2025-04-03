1bd865fb1e935559fa75f2e3456a66ba68d45aa3
Myeong sub <myeongsoup@imyeongseob-ui-MacBookAir.local>
2025-04-03 12:42:51 -0400

    내 로컬 변경사항 커밋

---
+++ EDGAR_API_Call.py
@@ -0,0 +1,8 @@
+import requests
+
+headers = {
+    'User-Agent': 'Jane Doe CompanyX (jane.doe@example.com)'
+}
+url = 'https://data.sec.gov/submissions/CIK0000320193.json'  # Example for Apple Inc.
+response = requests.get(url, headers=headers)
+print(response.json())
