e62358b174d978ac239ef842428def494493162b
Brian Lee <bjhlee@gmail.com>
2025-04-03 12:54:55 -0400

    Update EDGAR_API_Call.py

--- EDGAR_API_Call.py
+++ EDGAR_API_Call.py
@@ -6,3 +6,5 @@ headers = {
 url = 'https://data.sec.gov/submissions/CIK0000320193.json'  # Example for Apple Inc.
 response = requests.get(url, headers=headers)
 print(response.json())
+
+print("Hello World Out there!")
